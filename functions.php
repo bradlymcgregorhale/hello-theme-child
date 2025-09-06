fc86e506d339c61718dbf15d0babd6a8e194909d
bradlymcgregorhale <bradlymcgregorhale@gmail.com>
2025-09-06 11:28:47 -0300

    Use timestamp to update version

--- functions.php
+++ functions.php
@@ -23,14 +23,27 @@ define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
  */
 function hello_elementor_child_scripts_styles() {
 
+	$style_version = filemtime( get_stylesheet_directory() . '/style.css' );
 	wp_enqueue_style(
 		'hello-elementor-child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		[
 			'hello-elementor-theme-style',
 		],
-		HELLO_ELEMENTOR_CHILD_VERSION
+		$style_version
 	);
+
+	$script_path = get_stylesheet_directory() . '/scripts.js';
+	if ( file_exists( $script_path ) ) {
+		$script_version = filemtime( $script_path );
+		wp_enqueue_script(
+			'hello-elementor-child-scripts',
+			get_stylesheet_directory_uri() . '/scripts.js',
+			[],
+			$script_version,
+			true
+		);
+	}
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
---
+++ scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Hello Elementor Child Theme Scripts
+ */
+
+(function() {
+    'use strict';
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('Hello Elementor Child theme scripts loaded');
+    });
+    
+})();
